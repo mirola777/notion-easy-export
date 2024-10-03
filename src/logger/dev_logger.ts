b9f3c9dd8f29a28687376727ce1d3449710ddbf4
Mirola <jmmunoza@eafit.edu.co>
2024-10-03 14:18:33 -0500

    fix:Updating export sizes

--- src/logger/dev_logger.ts
+++ src/logger/dev_logger.ts
@@ -1,14 +1,9 @@
-import { Format } from "logform";
 import { createLogger, format, transports } from "winston";
 
 const devLogger = createLogger({
 	level: "debug",
 	format: format.json(),
-	transports: [
-		new transports.Console({ format: format.simple() }),
-		// new transports.File({ filename: 'error.log', level: 'error' }),
-		// new transports.File({ filename: 'combined.log' }),
-	],
+	transports: [new transports.Console({ format: format.simple() })],
 });
 
 export default devLogger;
